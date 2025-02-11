c842e9c1f3f2d9e8d7e5e396306c782eb0eef40f
Matheus Candido <candidoma050@gmail.com>
2025-02-10 21:54:35 -0300

    first commit

--- desafio1.cs
+++ desafio1.cs
@@ -1,14 +1,14 @@
 public class Solution {
-public bool DetectCapitalUse(string word)
-{
-    int upperCount = 0;
-    for (int i = 0; i < word.Length; i++)
+    public bool DetectCapitalUse(string word)
     {
-        if (char.IsUpper(word[i])) upperCount++;
-    }
+        int upperCount = 0;
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (char.IsUpper(word[i])) upperCount++;
+        }
 
-    return upperCount == 0 ||
-           upperCount == word.Length ||
-           (upperCount == 1 && char.IsUpper(word[0]));
-}
+        return upperCount == 0 ||
+            upperCount == word.Length ||
+            (upperCount == 1 && char.IsUpper(word[0]));
+    }
 }
